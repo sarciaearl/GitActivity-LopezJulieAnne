394076bcdb44750cf198ca4cbd032b0344fb40b7
earl <sarcia.jhonearl@mscmarinduque.edu.ph>
2024-03-08 13:49:48 +0800

    first-commit

---
+++ git-python-lopez.py
@@ -0,0 +1,6 @@
+print ("Julie Anne Lopez")
+print ("Pingan Gasan, Marinduque")
+print ("July 02, 2004")
+print ("College of Information and Computing Sciences")
+print ("Bachelor of Science in Information Technology")
+print ("2-B")
